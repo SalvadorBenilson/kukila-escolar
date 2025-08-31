e7d7f8b23c5500746fb748f1ff65825442186c79
SalvadorBenilson <benilsonsalvador16@gmail.com>
2025-08-31 20:50:08 +0100

    Primeiro  - Commit \ api manager container

---
+++ api-manager/app/Http/Controllers/EscolaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Escola;
+use Illuminate\Http\Request;
+
+class EscolaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Escola $escola)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Escola $escola)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Escola $escola)
+    {
+        //
+    }
+}
---
+++ api-manager/app/Http/Controllers/FuncionarioController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Funcionario;
+use Illuminate\Http\Request;
+
+class FuncionarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Funcionario $funcionario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Funcionario $funcionario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Funcionario $funcionario)
+    {
+        //
+    }
+}
